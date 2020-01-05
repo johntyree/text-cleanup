576bd346dc7e8485c2e244ae30b54ca8561eaac7
John Tyree <jtyree@google.com>
2020-01-04 20:34:38 -0800

    Start working on x(ht)ml support.

---
+++ text_cleanup/xml.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Cleanup xml text."""
+
+
+def get_nodes(xml):
+    return []
+
+
+def main():
+    """Run main."""
+
+    return 0
+
+if __name__ == '__main__':
+    main()
